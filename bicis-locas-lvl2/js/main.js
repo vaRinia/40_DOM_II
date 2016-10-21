2c0f126c6dc40bb9c75fc031ea9599b90d6252a6
vaRinia <varinia.ep@gmail.com>
2016-10-21 16:35:19 -0500

    avance

--- bicis-locas-lvl2/js/main.js
+++ bicis-locas-lvl2/js/main.js
@@ -1,4 +1,37 @@
+function validateForm() 
+{
+var nombre = document.getElementById("name");
+var apellido = document.getElementById("lastname");
+var emilio = document.getElementById("input-email");
+var contra = document.getElementById("input-password");
+var seleccion = document.getElementsByTagName("select")[0].selectedIndex;
+var regexEmilio = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+var regexNombreApellido = /^[ÑA-Za-z _]*[ñA-Za-z][ñA-Za-z _]*$/;
+var err = true;
+
+	if (nombre.value == null || nombre.value == "" || !regexNombreApellido.test(nombre.value)) 	{
+		var spane = document.createElement("span");
+		spane.innerHTML="Ingresa nombre, debe contener solo letras, primera letra mayúscula";
+		nombre.parentNode.appendChild(spane);
+		err = false;
+
+	}
+	if (appellido.value == null || apellido.value == "" || !regexNombreApellido.test(apellido.value)) {
+		var spane = document.createElement("span");
+		spane.innerHTML = "Ingresa apellido, solo letras, primera letra mayúscula";
+		apellido.parentNode.appendChild(spane);
+	}
+	if (emilio.value == null || emilio.value == "" || !regexEmilio(emilio.value)) {
+
+	}
+
+	return err;
+};
+
 /*
+
+<span>jlkjlkjlkjljlkjl</span>
+
 function validateForm() {
 	var nombre = document.getElementById("name").value;
 	if (nombre == "" || nombre == null) {
@@ -17,7 +50,7 @@ function mensaje (hermano,texto) {
 	return mensajeNegro;
 }
 
-*/
+
 
 /* NO LO USAREMOS AQUI, PERO PUEDE SERRR
 var boxes = document.getElementsByClassName("input-box");
@@ -28,12 +61,12 @@ for (var i = 0; i < boxes.length; i++) {
 */
 
 
-
+/*
 var nombre = document.getElementById("name"); //no necesitamos el string, sino el elemento
 if (nombre.value == "" || nombre.value == null) {
 	var span = document.createElement("span");
 	var texttoNombre = document.createTextNode("Llena este campo");
-	span.innerHTML = "Llena este campo"
+	span.innerHTML = 	
 }
 
 
@@ -42,3 +75,4 @@ var spans = document.getElementsByTagName("span");
 for (var i = 0; i < spans.lenght; i++) {
 	spans[i].parentNode.removeChild(spans[i]);
 }
+*/
